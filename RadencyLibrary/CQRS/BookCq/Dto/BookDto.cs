744cfcbbad754812d67e7223d4324e9a81966811
Miach-A <ovmiach@gmail.com>
2023-02-25 16:24:35 +0200

    add cover

--- RadencyLibrary/CQRS/BookCq/Dto/BookDto.cs
+++ RadencyLibrary/CQRS/BookCq/Dto/BookDto.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public decimal Rating { get; set; }
+        public string Cover { get; set; } = string.Empty;
         public int ReviewsNumber { get; set; }
 
     }
